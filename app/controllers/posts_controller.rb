e4a10a7b0efb354847704405b5a97076580ef079
Taiki Noda <taiki.nd.real@gmail.com>
2021-11-28 14:21:26 +0900

    ページネーションの適用

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.page(params[:page]).per(5)
   end
 
   # GET /posts/1 or /posts/1.json
